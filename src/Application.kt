b82260024ad2800cc131525378007cd836faebfd
gmidorii <stitow14@gmail.com>
2019-03-23 17:39:51 +0900

    Add singnup handler.
    - receive form paramter.

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,15 @@
 package gmidori.com.github
 
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.http.ContentType
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveParameters
+import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.route
 import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -18,6 +21,17 @@ fun Application.module(testing: Boolean = false) {
         routing{
             get("/") {
                 call.respondText("Hello World!!", ContentType.Text.Html)
+            }
+            get("/signup") {
+                val postParameters = call.receiveParameters()
+                val name = postParameters.get("name")
+                val bankId = postParameters.get("bank_id")
+                val password = postParameters.get("password")
+
+                if (name == "" || bankId == "" || password == "") {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
             }
         }
     }
