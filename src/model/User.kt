7e3a3bab5980023a2d5a613eef77b29031484d72
gmidorii <stitow14@gmail.com>
2019-03-30 16:51:28 +0900

    Refactor rename val.

--- src/model/User.kt
+++ src/model/User.kt
@@ -13,19 +13,19 @@ object User: Table(name = "user") {
     val createdAt: Column<DateTime> = datetime("created_at")
 }
 
-fun UserSignUp(pname: String, pbankId: String, ppassword: String) {
+fun UserSignUp(name: String, bankId: String, password: String) {
     //TODO: すでに登録しているかどうかを確認する
     val bankEndPoint = GetSetting("bank_endpoint")
     val bookAppId = GetSetting("bank_appid")
     val isuBank = IsuBank(bankEndPoint, bookAppId)
 
-    if (!isuBank.Check(pbankId)) {
-        //TODO: pbankId が不正のため処理を何もせずに返却
+    if (!isuBank.Check(bankId)) {
+        //TODO: bankId が不正のため処理を何もせずに返却
         return
     }
 
     val bcrypt = BCryptPasswordEncoder()
-    val hashedPassword = bcrypt.encode(ppassword)
+    val hashedPassword = bcrypt.encode(password)
     println(hashedPassword)
 
     Database.connect("jdbc:mysql://localhost:13306/isucoin?loc=Local&charset=utf8mb4",
@@ -34,8 +34,8 @@ fun UserSignUp(pname: String, pbankId: String, ppassword: String) {
         SchemaUtils.create(User)
 
         User.insertIgnore {
-            it[bankId] = pbankId.toByteArray()
-            it[User.userName] = pname
+            it[User.bankId] = bankId.toByteArray()
+            it[User.userName] = name
             it[User.password] = hashedPassword.toByteArray()
             it[User.createdAt] = DateTime.now()
         }
