2acfa97fd88764e2d42b23174790a900582c8f1a
gmidorii <stitow14@gmail.com>
2019-03-31 13:40:15 +0900

    Add response interface and impl class.

--- src/model/IsuBank.kt
+++ src/model/IsuBank.kt
@@ -1,8 +1,29 @@
 package gmidori.com.github.model
 
+interface IsuBankResponse {
+    fun setStatus(code: Int)
+}
+
+class IsubankBasicResponse(var code: Int): IsuBankResponse {
+    override fun setStatus(code: Int) {
+        this.code = code
+    }
+
+    fun success(): Boolean {
+       return code == 200
+    }
+}
+
+class IsubankReserveResponse(val basic: IsubankBasicResponse, var reserveId: Int) {
+}
+
 class IsuBank(val endPoint: String, val appId: String) {
     fun Check(bankId: String, price: Int = 0) : Boolean {
         //TODO: url requestしてcheckする
         return true
     }
+
+    fun request(path: String, body: String, r: IsuBankResponse) {
+    }
 }
+
